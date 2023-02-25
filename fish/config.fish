11da50ac9d090ba0b72b93bc7d3f19dd6ab8e50e
fedya-eremin <qwertier06@gmail.com>
2023-02-26 02:49:54 +0300

    1234

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,6 @@ if status is-interactive
 	pokemon-colorscripts -n charmander --no-title
 end
 
-if not status is-login
-	eval $(keychain --eval --quiet key) 
-end
 
 if status is-login
 	exec sway
@@ -19,13 +16,13 @@ alias listn="nmcli device wifi list"
 alias connect="nmcli device wifi connect "
 alias activate="source ./venv/bin/activate.fish"
 alias rmr="rm -r"
-alias keyinit="eval $(keychain --eval --quiet key)"
-alias keval="keychain --eval"
 alias mirrorscreen="wl-mirror -c HDMI-1 &"
 
 function mkcd
 	mkdir -p $argv;
 	cd $argv;
 end
-
+function kinit
+	eval $(keychain --quiet --eval $argv)
+end
 set PATH $PATH:/home/main/.local/bin
