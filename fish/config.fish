493538c019576d28be38840f58c18abca84bfb80
fedya-eremin <qwertier06@gmail.com>
2024-10-14 23:14:06 +0300

    ~~~

--- fish/config.fish
+++ fish/config.fish
@@ -2,21 +2,22 @@
 set fish_greeting
 if status is-interactive
 	pokemon-colorscripts -n charmander --no-title
+    # carapace setup (override builtin completions)
     carapace _carapace | source
     direnv hook fish | source
     pyenv init - | source
+    nvm use lts >> /dev/null
 end
 
 if status is-login
 and not set -q TMUX
-    # env XDG_CURRENT_DESKTOP=sway dbus-run-session sway
-    Hyprland
+    startx
 end
 
 # Silly tmux outlives sway
-if set -q TMUX
-    set -g SWAYSOCK /run/user/1000/sway-ipc.1000.$(pidof sway).sock
-end
+# if set -q TMUX
+#     set -g SWAYSOCK /run/user/1000/sway-ipc.1000.$(pidof sway).sock
+# end
 
 # NOTE: to get rid of poetry completion error, run:
 # poetry completions fish > ..../fish/vendor_completions.d/poetry.fish
@@ -48,10 +49,10 @@ alias vi="nvim"
 alias cicd="flake8 . && pytest"
 alias sudoe="sudoedit"
 alias feh="feh --scale-down --geometry 1000x800"
-# alias conda=" $HOME/anaconda3/bin/conda"
 alias info="info --vi-keys"
 alias battery="cat /sys/class/power_supply/BAT1/capacity"
 alias v="nvim"
+alias xi="sudo xbps-install"
 
 
 ### FUNCTIONS
@@ -133,6 +134,9 @@ end
 # opam configuration
 source /home/lemonade/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
+# cargo
+source "$HOME/.cargo/env.fish"
+
 # pnpm
 set -gx PNPM_HOME "/home/lemonade/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
@@ -143,3 +147,5 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+
---
+++ scripts/void-python.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo xbps-install bzip2-devel libffi-devel libxml2-devel readline readline-devel sqlite-devel zlib-devel python3-tkinter
+
